e94eac5a57cbce5b045e5244679ee5d510bd3c5f
Pascal Romanens <Pascal Romanens@192.168.1.231>
2012-09-18 16:30:22 +0200

    fdfd

---
+++ test/src/com/kalyss/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.kalyss;
+
+public class HelloWorld
+{
+
+	/**
+	 * <br /><b>Author:</b> Pascal Romanens
+	 * @param args
+	 * @since Sep 18, 2012
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println("hello world");
+	}
+
+}
